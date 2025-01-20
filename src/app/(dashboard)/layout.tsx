68852bda0ab35bc6380e03dbb1bdd8e37863e20b
jakir540 <jakirhossainmd540@gamil.com>
2025-01-20 19:46:26 +0600

    add side menu bar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,8 @@
+import Menu from "@/components/Menu";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +17,23 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <div>Dashboard Navbar</div>
-      <div>{children}</div>
-    </>
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold">IQRA</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        {/* <Navbar /> */}
+        {children}
+      </div>
+    </div>
   );
 }
